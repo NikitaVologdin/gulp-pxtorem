e0e3ef696bfe3ec4395caba240aa6da09eac6cc4
Nikita Vologdins <nvologdins@gmail.com>
2024-10-10 12:30:33 +0300

    replaced gulp-util

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
-var gutil = require('gulp-util');
-var through = require('through2');
-var postcss = require('postcss');
-var pxtorem = require('postcss-pxtorem');
+var PluginError = require("plugin-error");
+var through = require("through2");
+var postcss = require("postcss");
+var pxtorem = require("postcss-pxtorem");
 
-var PLUGIN_NAME = 'gulp-pxtorem';
+var PLUGIN_NAME = "gulp-pxtorem";
 
 module.exports = function (options, postcssOptions) {
-
     return through.obj(function (file, enc, cb) {
         if (file.isNull()) {
             this.push(file);
@@ -16,14 +15,22 @@ module.exports = function (options, postcssOptions) {
         }
 
         if (file.isStream()) {
-            this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            this.emit(
+                "error",
+                new PluginError(PLUGIN_NAME, "Streaming not supported")
+            );
             return cb();
         }
 
         try {
-            file.contents = new Buffer(postcss(pxtorem(options)).process(file.contents.toString(), postcssOptions).css);
+            file.contents = new Buffer(
+                postcss(pxtorem(options)).process(
+                    file.contents.toString(),
+                    postcssOptions
+                ).css
+            );
         } catch (err) {
-            this.emit('error', new gutil.PluginError(PLUGIN_NAME, err));
+            this.emit("error", new PluginError(PLUGIN_NAME, err));
         }
 
         this.push(file);
--- test.js
+++ test.js
@@ -6,40 +6,47 @@
  *      mocha
  */
 
- /* global describe, it */
+/* global describe, it */
 
-'use strict';
+"use strict";
 
-var assert = require('assert');
-var gutil  = require('gulp-util');
-var pxtorem = require('./');
+var assert = require("assert");
+var Vinyl = require("vinyl");
+var pxtorem = require("./");
 
-describe('gulp-pixrem', function() {
-    it('should postprocess CSS using gulp-pxtorem with the default pixel root value', function(done) {
+describe("gulp-pixrem", function () {
+    it("should postprocess CSS using gulp-pxtorem with the default pixel root value", function (done) {
         var stream = pxtorem();
 
-        stream.on('data', function(data) {
-            assert.equal(data.contents.toString(), 'code { font-size: 1rem; }');
+        stream.on("data", function (data) {
+            assert.equal(data.contents.toString(), "code { font-size: 1rem; }");
             done();
         });
 
-        stream.write(new gutil.File({
-            contents: new Buffer('code { font-size: 16px; }')
-        }));
+        stream.write(
+            new Vinyl({
+                contents: new Buffer("code { font-size: 16px; }"),
+            })
+        );
     });
 
-    it('should postprocess CSS using gulp-pxtorem with a custom pixel root value', function(done) {
+    it("should postprocess CSS using gulp-pxtorem with a custom pixel root value", function (done) {
         var stream = pxtorem({
-            root_value: 10
+            root_value: 10,
         });
 
-        stream.on('data', function(data) {
-            assert.equal(data.contents.toString(), 'code { font-size: 1.6rem; }');
+        stream.on("data", function (data) {
+            assert.equal(
+                data.contents.toString(),
+                "code { font-size: 1.6rem; }"
+            );
             done();
         });
 
-        stream.write(new gutil.File({
-            contents: new Buffer('code { font-size: 16px; }')
-        }));
+        stream.write(
+            new Vinyl({
+                contents: new Buffer("code { font-size: 16px; }"),
+            })
+        );
     });
 });
